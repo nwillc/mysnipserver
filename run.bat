a665e57b5cf7728933fe0bb7ec2d1dff510dae59
Nwillc <nwillc@gmail.com>
2015-10-09 11:50:18 -0400

    tweaks to bat file

--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
-@echo off
+@if "%DEBUG%" == "" @echo off
+@rem Basic build and start script.
+
+echo Rebuild server...
 call gradlew.bat -q clean stage
+
+echo Start server...
 java -cp build\staging;build\staging\* com.github.nwillc.mysnipserver.MySnipServer %*
