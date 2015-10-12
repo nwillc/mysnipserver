daca945b1b435ccd90d6d48d1a04d4dee394c1cb
Nicholas Christopher <nwillc@gmail.com>
2015-10-12 11:02:41 -0400

    titles hooked up. Categories filled on launch

--- src/main/resources/public/mysnipserver.js
+++ src/main/resources/public/mysnipserver.js
@@ -1,11 +1,24 @@
-  $(document).ready(function(){
-            $("button").click(function(){
-                $.get("v1/snippets/category/Java", function(data, status){
-                    var list = JSON.parse(data);
-                    var categories = $("#categories");
-                    $(list).each(function(){
-                       categories.append(new Option(this,this));
-                    });
-                });
-            });
+ $(document).ready(function(){
+    var ping = $("#ping");
+    var categories = $("#categories");
+    var titles = $("#titles");
+
+    $(categories).change(function() {
+        var category = $(categories).val();
+        console.log("Selected Category: " + category);
+        $.get("v1/snippets/category/" + category, function(data, status){
+            var list = JSON.parse(data);
+            $(list).each(function(){
+                titles.append(new Option(this,this));
+            })
+        })
+    });
+
+    $.get("v1/categories", function(data, status){
+        var list = JSON.parse(data);
+        $(list).each(function(){
+            categories.append(new Option(this,this));
         });
+    });
+
+ });
