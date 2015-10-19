5478d4b1d148622e4cfddff62c156cfc657ddb83
unknown <nchristopher1@Z146615.rjf.com>
2015-10-19 15:15:26 -0400

    Refactored javasceript

--- src/main/resources/public/mysnipserver.js
+++ src/main/resources/public/mysnipserver.js
@@ -1,75 +1,108 @@
-function LoadCategories() {
-    var categories = $("#categories");
-    $.get("v1/categories", function (data, status) {
-        var list = JSON.parse(data);
-        $(categories).empty();
-        $(list)
-            .sort(function (a, b) {
-                return a.name > b.name;
-            })
-            .each(function () {
-                categories.append(new Option(this.name, this.name));
-            });
-    });
-}
+var myPresentation = {
 
-$(document).ready(function () {
-    var ping = $("#ping");
-    var categories = $("#categories");
-    var titles = $("#titles");
-    var body = $("#body");
+    init: function (config) {
+        console.log("init");
+        myPresentation.config = {
+            categories: $('#categories'),
+            titles: $('#titles'),
+            body: $('#body')
+        };
+        $.extend(myPresentation.config, config);
+        myPresentation.bind();
+    },
 
-    $("div").hide();
+    bind: function () {
+        console.log("bind");
+        myPresentation.hideAll();
+        $('#browseButton').click(myPresentation.showBrowse);
+        $('#newSnippetButton').click(myPresentation.showNewSnippet);
+        $('#newCategoryButton').click(myPresentation.showNewCategory);
+        $(myPresentation.config.categories).change(myPresentation.loadTitles);
+        $(myPresentation.config.titles).change(myPresentation.loadBody);
+        $("#category").change(myPresentation.saveCategory);
+        $('#bodyInput').change(myPresentation.saveSnippet);
+    },
 
-    $("#browseButton").click(function () {
-        $("div").hide();
-        LoadCategories();
+    hideAll: function () {
+        $('div').hide();
+    },
+
+    showBrowse: function () {
+        console.log('showBrowse');
+        myPresentation.hideAll();
+        myPresentation.loadCategories();
         $("#categoryDiv").show();
         $("#browseDiv").show();
-    });
+    },
 
-    $("#newSnippetButton").click(function () {
-        $("div").hide();
-        LoadCategories();
+    showNewSnippet: function () {
+      console.log("showNewSnippet");
+        myPresentation.hideAll();
+        myPresentation.loadCategories();
         $("#categoryDiv").show();
         $("#newSnippetDiv").show();
-    });
+    },
 
-    $("#newCategoryButton").click(function () {
-        $("div").hide();
+    showNewCategory: function () {
+        console.log("showNewCategory");
+        myPresentation.hideAll();
         $("#newCategoryDiv").show();
-    });
+    },
 
-    $(categories).change(function () {
-        var category = $(categories).val();
+    loadCategories: function () {
+        console.log("loadCategories");
+        $.get("v1/categories", function (data, status) {
+            var list = JSON.parse(data);
+            $(myPresentation.config.categories).empty();
+            $(list)
+                .sort(function (a, b) {
+                    return a.name > b.name;
+                })
+                .each(function () {
+                    myPresentation.config.categories.append(new Option(this.name, this.name));
+                });
+        });
+    },
+
+    loadTitles: function () {
+        console.log("loadTitles");
+        var category = $(myPresentation.config.categories).val();
         console.log("Selected Category: " + category);
-        $('option', '#titles').remove();
-        $(body).val('');
+        $('option', myPresentation.config.titles).remove();
+        $(myPresentation.config.body).val('');
         $.get("v1/snippets/category/" + category, function (data, status) {
             var list = JSON.parse(data);
             $(list).each(function () {
-                titles.append(new Option(this.title, this.title));
+                myPresentation.config.titles.append(new Option(this.title, this.title));
             })
         })
-    });
+    },
 
-    $(titles).change(function () {
-        var category = $(categories).val();
-        var title = $(titles).val();
+    loadBody: function () {
+        console.log("loadBody");
+        var category = $(myPresentation.config.categories).val();
+        var title = $(myPresentation.config.titles).val();
         console.log("Selected Category: " + category + " Title: " + title);
         $.get("v1/snippets/category/" + category + "/title/" + title, function (data, status) {
             console.log("Status: " + status + " Data: " + data);
             var found = JSON.parse(data);
-            $(body).val(found.body);
+            $(myPresentation.config.body).val(found.body);
         })
-    });
+    },
 
-    $("#category").change(function () {
+    saveCategory: function () {
+        console.log("saveCategory");
         $.post("v1/categories", JSON.stringify({name: $("#category").val()}));
         $('#category').val('');
-    });
+    },
 
-    $('#bodyInput').change(function () {
+    saveSnippet: function () {
+        console.log("Save Snippet");
         $.post('v1/snippets', JSON.stringify({category: 'Java', title: 'default method', body: 'This is the body'}));
-    });
+    }
+
+};
+
+$(document).ready(function () {
+    myPresentation.init();
 });
