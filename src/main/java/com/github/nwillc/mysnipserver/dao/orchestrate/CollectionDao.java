d2c34a0dd4c982671e47360accda4bd98ed34b38
nwillc <nwillc@gmail.com>
2015-11-19 11:22:42 -0500

    revert

--- src/main/java/com/github/nwillc/mysnipserver/dao/orchestrate/CollectionDao.java
+++ src/main/java/com/github/nwillc/mysnipserver/dao/orchestrate/CollectionDao.java
@@ -40,7 +40,6 @@ import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -55,7 +54,6 @@ public class CollectionDao<T extends Entity> implements Dao<T> {
     private final String collection;
     private final Client client;
     private final Cache<String, T> cache;
-    private final AtomicBoolean loaded = new AtomicBoolean(false);
 
     public CollectionDao(Client client, Class<T> tClass) {
         this(client, tClass.getSimpleName(), tClass);
@@ -75,16 +73,12 @@ public class CollectionDao<T extends Entity> implements Dao<T> {
 
     @Override
     public Stream<T> findAll() {
-        if (loaded.compareAndSet(false, true)) {
-            Set<String> keys = stream(client.listCollection(collection)
-                    .limit(LIMIT)
-                    .withValues(false)
-                    .get(tClass)
-                    .get().spliterator(), false).map(KvObject::getKey).collect(Collectors.toSet());
-            return find(keys);
-        } else {
-            return stream(cache.spliterator(), false).map(e -> cache.get(e.getKey()));
-        }
+        Set<String> keys = stream(client.listCollection(collection)
+                .limit(LIMIT)
+                .withValues(false)
+                .get(tClass)
+                .get().spliterator(), false).map(KvObject::getKey).collect(Collectors.toSet());
+        return find(keys);
     }
 
     @Override
