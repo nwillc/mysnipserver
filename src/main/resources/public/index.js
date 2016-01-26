29ace5eff7c52c416c7d52f5ab49d5bfae692cb6
nwillc <nwillc@gmail.com>
2016-01-26 11:45:47 -0500

    Tidying

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -23,10 +23,10 @@ function gapiAuth2Load () {
 }
 
 function gapiSignOut() {
+    APP.home.logout();
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out of Google.');
-        APP.home.logout();
     });
 }
 
