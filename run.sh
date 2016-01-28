495392b0b936f19784fa99c5f9f908caebf7e5b6
nwillc <nwillc@gmail.com>
2016-01-28 11:39:22 -0500

    Updateing gradle version

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ cd ${SCRIPT_DIR}
 [ -f env.sh ] && source env.sh
 
 echo Rebuild server...
-gradle -q stage -x test
+./gradlew -q stage -x test
 
 LIB_DIR=build/staging
 IFS=":"
@@ -15,8 +15,6 @@ if [[ ${OSTYPE} == "cygwin" ]] ; then
 	IFS=";"
 fi
 export CLASSPATH=${LIB_DIR}$(JARS=(${LIB_DIR}/*.jar); IFS=${IFS} ; echo "${JAR_FILE}${IFS}${JARS[*]}")
-
-echo "${CLASSPATH}"
 
 echo Start server...
 java -Djava.awt.headless=true com.github.nwillc.mysnipserver.MySnipServer $*
