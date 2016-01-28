a7b97b373d095e0fb290bf96cbe51bf50ee8f7e9
nwillc <nwillc@gmail.com>
2016-01-28 10:44:55 -0500

    Messing with lf

--- run.sh
+++ run.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(cd $(dirname ${0}) && pwd -P)
-
-cd ${SCRIPT_DIR}
-
-[ -f env.sh ] && source env.sh
-
-echo Rebuild server...
-./gradlew -q stage -x test
-
-echo Start server...
-java -cp build/staging:build/staging/* -Djava.awt.headless=true com.github.nwillc.mysnipserver.MySnipServer $*
-
+#!/bin/bashSCRIPT_DIR=$(cd $(dirname ${0}) && pwd -P)cd ${SCRIPT_DIR}[ -f env.sh ] && source env.shecho Rebuild server..../gradlew -q stage -x testecho Start server...java -cp build/staging:build/staging/* -Djava.awt.headless=true com.github.nwillc.mysnipserver.MySnipServer $*
