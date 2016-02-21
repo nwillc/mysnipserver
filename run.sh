73b30c9d21a6a9b74d4e22e5f0d6bdd7a6eec822
Nicholas Christopher <nwillc@gmail.com>
2016-02-21 12:41:14 -0500

    Simplified classpath

--- run.sh
+++ run.sh
@@ -9,13 +9,6 @@ cd ${SCRIPT_DIR}
 echo Rebuild server...
 ./gradlew -q stage -x test
 
-LIB_DIR=build/staging
-IFS=":"
-if [[ ${OSTYPE} == "cygwin" ]] ; then
-	IFS=";"
-fi
-export CLASSPATH=${LIB_DIR}$(JARS=(${LIB_DIR}/*.jar); IFS=${IFS} ; echo "${JAR_FILE}${IFS}${JARS[*]}")
-
 echo Start server...
-java -Djava.awt.headless=true com.github.nwillc.mysnipserver.MySnipServer $*
+java  -cp build/staging:build/staging/* -Djava.awt.headless=true com.github.nwillc.mysnipserver.MySnipServer $*
 
