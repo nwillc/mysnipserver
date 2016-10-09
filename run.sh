7cc0c90c2d30b75d04393361650b6e50ce870803
Nicholas Christopher <nwillc@gmail.com>
2016-10-09 00:43:52 -0400

    Cleaning

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ cd ${SCRIPT_DIR}
 echo Rebuild server...
 ./gradlew -q clean oneJar -x test
 
-JAVA_OPTS=-Djava.awt.headless=true -XX:+UnlockCommercialFeatures -XX:+FlightRecorder
+JAVA_OPTS="-Djava.awt.headless=true -XX:+UnlockCommercialFeatures -XX:+FlightRecorder"
 
 echo Start server...
 java ${JAVA_OPTS} -jar build/libs/*-standalone.jar $*
