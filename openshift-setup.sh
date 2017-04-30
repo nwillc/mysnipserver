b5610b7be97d1252423406df7bdc91d83cb52fe6
Nicholas Christopher <nwillc@gmail.com>
2017-04-30 16:16:22 -0400

    Good by openshift

--- openshift-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-. ./env.sh
-
-# rhc env set ORCH_API_KEY=${ORCH_API_KEY} CLIENT_ID=${CLIENT_ID} -a snippets
-rhc env set MONGO_DB_SERVER=${MONGO_DB_SERVER} MONGO_DB_PORT=${MONGO_DB_PORT} -a snippets
-rhc env set MONGO_DB_USER=${MONGO_DB_USER} MONGO_DB_PASSWORD=${MONGO_DB_PASSWORD} -a snippets
