bc08274d9cc297ab24201d78c43f6cb40e4d2167
Nicholas Christopher <nwillc@gmail.com>
2017-10-14 20:27:22 -0400

    OPA update

--- src/main/java/com/github/nwillc/mysnipserver/util/guice/MongoDbModule.java
+++ src/main/java/com/github/nwillc/mysnipserver/util/guice/MongoDbModule.java
@@ -16,12 +16,12 @@
 
 package com.github.nwillc.mysnipserver.util.guice;
 
-import com.github.nwillc.mysnipserver.dao.mongodb.MongoDbDao;
 import com.github.nwillc.mysnipserver.entity.Category;
 import com.github.nwillc.mysnipserver.entity.Snippet;
 import com.github.nwillc.mysnipserver.entity.User;
 import com.github.nwillc.opa.CachingDao;
 import com.github.nwillc.opa.Dao;
+import com.github.nwillc.opa.mongo.MongoDbDao;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.TypeLiteral;
@@ -38,19 +38,20 @@ public class MongoDbModule implements Module {
     private static final String MONGO_DB_PORT = System.getenv("MONGO_DB_PORT");
     private static final String MONGO_DB_USER = System.getenv("MONGO_DB_USER");
     private static final String MONGO_DB_PASSWORD = System.getenv("MONGO_DB_PASSWORD");
+    private static final String DATABASE = "snippets";
 
     @Override
     public void configure(Binder binder) {
         Logger.info("Configuring MongoDb Module");
         ServerAddress serverAddress = new ServerAddress(MONGO_DB_SERVER, Integer.parseInt(MONGO_DB_PORT));
         MongoCredential credential = MongoCredential.createCredential(MONGO_DB_USER,
-                "snippets",
+                DATABASE,
                 MONGO_DB_PASSWORD.toCharArray());
         List<MongoCredential> auths = Collections.singletonList(credential);
         MongoClient client = new MongoClient(serverAddress, auths);
-        Dao<String, Category> categoryDao = new MongoDbDao<>(client, Category.class);
-        Dao<String, Snippet> snippetDao = new CachingDao<>(new MongoDbDao<>(client, Snippet.class));
-        Dao<String, User> userDao = new MongoDbDao<>(client, User.class);
+        Dao<String, Category> categoryDao = new MongoDbDao<>(client, DATABASE, Category.class);
+        Dao<String, Snippet> snippetDao = new CachingDao<>(new MongoDbDao<>(client, DATABASE, Snippet.class));
+        Dao<String, User> userDao = new MongoDbDao<>(client, DATABASE, User.class);
         binder.bind(new TypeLiteral<Dao<String, Category>>(){}).toInstance(categoryDao);
         binder.bind(new TypeLiteral<Dao<String, Snippet>>(){}).toInstance(snippetDao);
         binder.bind(new TypeLiteral<Dao<String, User>>(){}).toInstance(userDao);
